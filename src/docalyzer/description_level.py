bfb1a7566761eec6e2a0969ba4943a13fcd005ea
vpro3611 <vprov8645@gmail.com>
2026-07-02 13:47:11 +0200

    add: define description and summary level prompt mappings

---
+++ src/docalyzer/description_level.py
@@ -0,0 +1,5 @@
+DESCRIPTION_LEVEL_TO_PROMPT = {
+    1: "Provide a concise description of the given text.",
+    2: "Provide a detailed description of the given text.",
+    3: "Provide an in-depth and very detailed description of the given text.",
+}
---
+++ src/docalyzer/summary_level.py
@@ -0,0 +1,5 @@
+SUMMARY_LEVEL_TO_PROMPT = {
+    1: "The summary should be concise, but still fit into the sentences cap",
+    2: "The summary should be detailed, but still fit into the sentences cap",
+    3: "The summary should be very detailed and in-depth, but still fit into the sentences cap",
+}
